56201e2c9000a9826de82db10d82956140b6d23a
Hyman <hymannnn@163.com>
2024-02-24 18:58:17 +0800

    二叉树的基本操作

--- chapter6/BinaryTree/BinaryTree/Header.h
+++ chapter6/BinaryTree/BinaryTree/Header.h
@@ -8,15 +8,130 @@
 #ifndef Header_h
 #define Header_h
 #define ElementType int
+#define MaxSize 10
+
+// 定义堆栈
+typedef struct sNode *Stack;
+struct sNode{
+    ElementType data[MaxSize];
+    int top;
+};
+
+//// 定义树和二叉树
+//typedef struct TreeNode *Tree;
+//struct TreeNode{
+//    ElementType Data;
+//    Tree Left;
+//    Tree Right;
+//};
 
 // 定义二叉树
-typedef struct TreeNode *BinTree;
-struct TreeNode{
+typedef struct TreeNode1 *BinTree;
+typedef BinTree Position;
+struct TreeNode1{
     ElementType Data;
     BinTree Left;
     BinTree Right;
 };
+// 二叉树查找操作
+Position Find(ElementType X, BinTree BST){
+    if (!BST) return NULL;
+    if (X<BST->Data) return Find(X, BST->Left);
+    else if (X>BST->Data) return Find(X, BST->Right);
+    else return BST;
+}
+// 改为迭代查找
+Position IterFind(ElementType X, BinTree BST){
+    while (BST){
+        if (X > BST->Data) BST = BST->Right;
+        else if (X<BST->Data) BST = BST ->Left;
+        else return BST;
+    }
+    return BST;
+}
+//递归FindMin
+Position FindMin(BinTree BST){
+    if (!BST) return NULL;
+    else if (BST->Left == NULL) return BST;
+    else return FindMin(BST->Left);
+}
+//迭代FindMin
+Position IterFindMin(BinTree BST){
+    while(BST){
+        if (BST->Left == NULL) return BST;
+        else BST = BST->Left;
+    }
+    return NULL;
+}
+Position IterFindMax(BinTree BST){
+    if(BST){
+        while (BST->Right) BST = BST->Right;
+    }
+    return BST;
+    
+}
 
+//Insert
+BinTree Insert(ElementType X, BinTree BST){
+    if (!BST){
+        BST = malloc(sizeof(struct TreeNode1));
+        BST ->Data = X;
+        BST->Left = BST->Right= NULL;
+    }
+    else {
+        if (X < BST->Data)
+            BST ->Left = Insert(X, BST);
+        else if (X > BST -> Data)
+            BST ->Right = Insert(X, BST);
+    }
+    return BST;
+}
+////Delete
+//BinTree Delete(ElementType X, BinTree BST){
+//    Position Tmp;
+//    if (!BST) printf("要删除的元素为找到");
+//    else if (X<BST->Data)
+//        BST->Left = Delete(X, BST);
+//    else if (X>BST->Data)
+//        BST->Right = Delete(X, BST);
+//    else
+//        if (BST->Left && BST->Right){
+//            Tmp = FindMin(BST->Right);
+//            BST->Data = Tmp->Data;
+//        }
+//        else Tmp = BST;
+//        if (BST->Left) BST = BST->Left;
+//         else if (BST->Right) BST = BST->Right;
+//        
+//        
+//}
+
+//// 构造空树
+//void InitTree(Tree *T){
+//    (*T) = NULL;
+//}
+
+//// 销毁树
+//void DestroyTree(Tree *T){
+//    if (*T)
+//    {
+//        DestroyTree( &( *T ) -> Left );
+//        DestroyTree( &( *T ) -> Right );
+//        (*T) = NULL;
+//        free((*T));
+//    }
+//    return;
+//}
+
+//// 创建树
+//void CreateTreee(Tree *T){
+//    (*T) = (Tree)malloc(sizeof(struct TreeNode));
+//}
+// 二叉树的判空
+int IsEmpty(BinTree BT){
+    if (BT == NULL) return 1;
+    else return 0;
+}
 // 先序遍历
 void PreOrderTraversal(BinTree BT){
     if (BT){printf("%d",BT->Data);
@@ -24,7 +139,6 @@ void PreOrderTraversal(BinTree BT){
         PreOrderTraversal(BT->Left);
     }
 }
-
 // 中序遍历
 void InOrderTraversal0(BinTree BT){
     if (BT){
@@ -33,7 +147,6 @@ void InOrderTraversal0(BinTree BT){
         InOrderTraversal0(BT->Left);
     }
 }
-
 // 后序遍历
 void PostOrderTraversal(BinTree BT){
     if (BT){
@@ -42,17 +155,4 @@ void PostOrderTraversal(BinTree BT){
         printf("%d",BT->Data);
     }
 }
-
-int IsEmpty(BinTree BT){
-    if (BT->Left==NULL && BT->Right==NULL) return 1;
-    else return 0;
-}
-
-// 非递归中序遍历
-//void InOrderTraversal(BinTree BT){
-//    BinTree T = BT;
-//    if (!IsEmpty(BT)) {
-//        if (BT->Left){
-//    }
-//}
 #endif /* Header_h */
--- chapter6/BinaryTree/BinaryTree/main.c
+++ chapter6/BinaryTree/BinaryTree/main.c
@@ -6,9 +6,13 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "Header.h"
 
 int main(int argc, const char * argv[]) {
-    
+    // 创建一个数T
+    Tree T = malloc(sizeof(struct TreeNode));
+    T->Data = 0;
+    T->Left = NULL;
     return 0;
 }
