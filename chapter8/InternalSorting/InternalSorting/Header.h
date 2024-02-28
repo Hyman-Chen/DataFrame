9b0632fbad1826739e6a3df1ec242216077f1c35
Hyman <hymannnn@163.com>
2024-02-28 21:56:35 +0800

    冒泡排序

--- chapter8/InternalSorting/InternalSorting/Header.h
+++ chapter8/InternalSorting/InternalSorting/Header.h
@@ -8,10 +8,23 @@
 #ifndef Header_h
 #define Header_h
 #
-typedef
+typedef int ElementType;
+
+void Swap(ElementType A, ElementType B){
+    
+}
 // 冒泡排序
 void Bubble_Sort(ElementType A[], int N)
-{
-    for 
+{ int P, i, flag;
+    for (P=N-1; P>=0;P--){
+        flag = 0;
+        for (i =0; i<P;i++){
+            if (A[i] >A[i+1]){
+                Swap(A[i], A[i+1]);
+                flag =1;
+            }
+            if (flag ==0 ) break;
+        }
+    }
 }
 #endif /* Header_h */
